cc4685c820943510e5fb30f41d46bb30933eb281
Jost Mawick <j.mawick03@gmail.com>
2024-12-16 18:17:10 +0100

    hier funktioniert nichts mehr will wieder zurück :(

--- include/blynk_connection.h
+++
@@ -1,7 +0,0 @@
-#ifndef BLYNK_CONNECTION_H
-#define BLYNK_CONNECTION_H
-
-void initBlynk();
-void sendMoistureToBlynk(int moisture);
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
-#include "wifi_connection.h"
-#include "web_server.h"
+#include "display.h"
+#include "moisture.h"
 #include "blynk_connection.h"
 
 void setup() {
     Serial.begin(115200);
-    connectToWiFi();
-    initBlynk();
-    startWebServer();
+    initDisplay();
+    showText("Bodenfeuchte");
 }
 
 void loop() {
-    int moisture = analogRead(34); // Beispiel für das Auslesen des Feuchtigkeitswerts
-    updateMoistureValue(moisture);
-    sendMoistureToBlynk(moisture); // Feuchtigkeitswert an Blynk senden
-    delay(1000);
+    showText("Feuchtigkeit:   %");
+    int moisture = readMoisture(); // Bodenfeuchtigkeit in Prozent auslesen
+    showMoist_Value(moisture); // Bodenfeuchte am Display anzeigen
+   // sendMoistureToBlynk(moisture); // Bodenfeuchte an Blynk senden
+    delay(100);
 }
+
+
+   
