75bfcc028c5bc96f57d48bdbada760dce0a9ead6
Jost Mawick <j.mawick03@gmail.com>
2024-12-17 18:50:39 +0100

    Remove unused display, moisture, web server, and WiFi connection code

--- include/display.h
+++
@@ -1,7 +0,0 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
-
-void initDisplay();
-void showText(const char* message);
-//void showMoist_Value(int m_Value)
-#endif
--- include/wifi_connection.h
+++
@@ -1,9 +0,0 @@
-#ifndef WIFI_CONNECTION_H
-#define WIFI_CONNECTION_H
-
-extern const char* ssid;
-extern const char* password;
-
-void connectToWiFi();
-
-#endif
